9e9279179f50525c6ba894bebd9043c0f59ab134
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-10-31 00:09:54 -0300

    WidgetForm

---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,21 @@
+export function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe o seu feedback</span>
+      </header>
+
+      <p>Hello world</p>
+
+      <footer className="text-xs text-neutral-600">
+        Feito com ♥ pela{' '}
+        <a
+          className="underline underline-offset-2"
+          href="http://www.github.com/vitoriapac"
+        >
+          Vitoria Pacheco
+        </a>
+      </footer>
+    </div>
+  )
+}
