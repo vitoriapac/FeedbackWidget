4b39255c29ee5d657e86e1d5558b237627081be5
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-11-14 23:27:01 -0300

    Send mail

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'ce4995752a6a64',
+    pass: '69a3f2c72716ad'
+  }
+})
 
 //Create Feedback
 app.post('/feedbacks', async (req, res) => {
@@ -15,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Vitoria Pacheco <vitoriac.pac@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size:16px; color:#252525">`,
+      `<p>Tipo de Feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   })
 
   return res.status(201).json({ data: feedback })
